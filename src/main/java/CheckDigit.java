8adfec2904401d909634bb5115daa31be431e338
aslieberman <aslieberman@s.sfusd.edu>
2025-05-01 22:56:06 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -5,34 +5,24 @@ public class CheckDigit
    *  six, inclusive.  
    *          num >= 0  
    */  
-   public static int getCheck(int num) 
-   {  
-     int digits = getNumberOfDigits(num);
-     int sum = 0;
-     for (int i = 1; i <= digits; i++)
-     {
-       int d = getDigit(num, i);
-       if (i % 2 == 0)
-         sum += 2 * d;
-       else
-         sum += 3 * d;
-     }
-     int rem = sum % 10;
-     return (rem == 0) ? 0 : 10 - rem;
-   }
- 
-  /** Returns true if numWithCheckDigit is valid, or false    
-   *  otherwise, as described in part (b). 
-   *  Precondition: The number of digits in numWithCheckDigit   
-   *  is between two and seven, inclusive.
-   *                numWithCheckDigit >= 0     
-   */     
-   public static boolean isValid(int numWithCheckDigit)    
-   {      
-     int check = numWithCheckDigit % 10;
-     int num = numWithCheckDigit / 10;
-     return getCheck(num) == check;    
-   }    
+  public static int getCheck(int num){
+    int digits = getNumberOfDigits(num);
+    int sum = 0;
+    for (int i = 1; i <= digits; i++)
+    {
+      int weight = 8 - i;           
+      sum += getDigit(num, i) * weight;
+    }
+    return sum % 10;         
+  }
+  
+  public static boolean isValid(int numWithCheckDigit)
+  {
+    int checkDigit = numWithCheckDigit % 10;
+    int num = numWithCheckDigit / 10;
+    return getCheck(num) == checkDigit;
+  }
+
    
    /** Returns the number of digits in num. */    
    public static int getNumberOfDigits(int num)    
